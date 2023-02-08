9f0629d773f5829a5c01560ae3f03c2d0a494f72
marianagiraldom <123570078+marianagiraldom@users.noreply.github.com>
2023-02-08 10:24:53 -0500

    08/02

---
+++ app.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cardsAdj = [
+    {
+      name: "ariel",
+      img: "imagenes/ariel.png"
+    },
+    {
+      name: "aurora",
+      img: "imagenes/aurora.png"
+    },
+    {
+      name: "bella",
+      img: "imagenes/bella.png"
+    },
+    {
+      name: "blancanieves",
+      img: "imagenes/blancanieves.png"
+    },
+    {
+      name: "cenicienta",
+      img: "imagenes/cenicienta.png"
+    },
+    {
+      name: "rapunzel",
+      img: "imagenes/rapunzel.png"
+    },
+
+    {
+      name: "ariel",
+      img: "imagenes/ariel.png"
+    },
+    {
+      name: "aurora",
+      img: "imagenes/aurora.png"
+    },
+    {
+      name: "bella",
+      img: "imagenes/bella.png"
+    },
+    {
+      name: "blancanieves",
+      img: "imagenes/blancanieves.png"
+    },
+    {
+      name: "cenicienta",
+      img: "imagenes/cenicienta.png"
+    },
+    {
+      name: "rapunzel",
+      img: "imagenes/rapunzel.png"
+    }
+  ];
+});
