839dd5678ac0f5e7c19f5c8b02c3fe81cd0a1c3e
marianagiraldom <123570078+marianagiraldom@users.noreply.github.com>
2023-02-13 11:54:27 -0500

    13/02

--- app.js
+++ app.js
@@ -50,4 +50,23 @@ document.addEventListener("DOMContentLoaded", () => {
       img: "imagenes/rapunzel.png"
     }
   ];
+
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasEscogidas = [];
+  var cartasEscogidasId = [];
+  var cartasGanadas = [];
+
+  //----------------- lecture_03 ----------------------------------//
+  function crearTablero() {
+    for (let i = 0; i < cardsAdj.length; i++) {
+      var carta = document.createElement("img");
+      carta.setAttribute("src", "images/reverso.png");
+
+      carta.setAttribute("data-id", i);
+      carta.addEventListener("click", voltearCarta);
+
+      cuadricula.appendChild(carta);
+    }
+  }
 });
